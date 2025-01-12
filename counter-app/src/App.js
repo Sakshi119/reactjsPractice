0e656a1cbfc60a31b4cad83a9cb1c6ee4c6d0b28
Sakshi <sensakshi5@gmail.com>
2025-01-12 18:44:42 +0530

    todo app

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/counterApp/counter';
-
+import TodoApp from './Components/toDoList/todo-list';
 
 function App() {
   return (
+    <>
     <div className="app">
-    <h1>Simple Counter App</h1>
-    <Counter />
-  </div>
+      <h1>Simple Counter App</h1>
+      <Counter />    
+    </div>
+
+    <div className='app2'>
+      <TodoApp />
+    </div>
+    </>
+    
   );
 }
 

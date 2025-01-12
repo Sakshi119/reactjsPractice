ab43fb7ecddd92d9bbe9d7c4e0ef4097dfb73180
Sakshi <sensakshi5@gmail.com>
2025-01-12 16:55:44 +0530

    counter app completed

---
+++ counter-app/src/Components/counterApp/counter.js
@@ -0,0 +1,22 @@
+import React,{useState} from "react";
+import "./counter.css";
+function Counter(){
+const [count,setCount] = useState(0);
+
+const increment = () => {setCount(count + 1)};
+const decrement = () => setCount(count - 1);
+const reset = () => setCount(0);
+
+return(
+    <div className="counter">
+      <h2>Counter: {count}</h2>
+      <div className="buttons">
+        <button onClick={increment} >Increment</button>
+        <button onClick={decrement} className={count === 0 ? "disabled" : ""}>Decrement</button>
+        <button onClick={reset} className={count === 0 ? "disabled" : ""}>Reset</button>
+      </div>
+    </div>
+);
+}
+
+export default Counter;
